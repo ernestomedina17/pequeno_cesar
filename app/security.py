b04a1c0def59651bbc8179cc29e3ffd1066535da
ernestomedina17 <ernestomedina17@gmail.com>
2020-04-17 16:32:27 -0500

    prefer original jwt-extended error handling over custom messages

--- app/security.py
+++ app/security.py
@@ -36,21 +36,6 @@ def configure_JWTManager(jwt, tokens_blacklist):
             return {'role': 'admin'}  # PUT, GET & DELETE
         return {'role': 'consumer'}  # Only GETs
 
-    # @jwt.unauthorized_loader
-    # def missing_token_callback():
-    #     return {'description': 'Request does not contain an access token.',
-    #             'error': 'authorization_required'}, 401
-    #
-    # @jwt.needs_fresh_token_loader
-    # def token_not_fresh_callback():
-    #     return {'description': 'The token is not fresh',
-    #             'error': 'fresh_token_required'}, 401
-    #
-    # @jwt.revoked_token_loader
-    # def revoked_token_callback():
-    #     return {'description': 'The token has been revoked.',
-    #             'error': 'token_revoked'}, 401
-
     @jwt.token_in_blacklist_loader
     def check_if_token_in_blacklist(decrypted_token):
         jti = decrypted_token['jti']
