920fd9313a81e8ba3740dd7b8181b923b9d5c53e
ernestomedina17 <ernestomedina17@gmail.com>
2020-05-01 19:29:47 -0500

    implementing query metrics

--- app/metrics.py
+++ app/metrics.py
@@ -1,5 +1,18 @@
 from prometheus_client import Summary, Histogram, Gauge, Counter
 
+# 4 types of Metrics:
+# - Counter: goes up only, like website visits.
+# - Gauge:  goes up or down, cpu or temperature.
+# - Histogram: observations, request duration(latency), response sizes. Observations get counted into buckets.
+#   Includes (_count and _sum). Main purpose is calculating quantiles.
+# - Summary: observations, request duration, response sizes. ALso includes TOTAL sum of observed values,
+#   it calculates quantiles over a sliding time window.
+#
+# Two rules of thumb:
+# If you need to aggregate, choose histograms.
+# Otherwise, choose a histogram if you have an idea of the range and distribution of values that will be observed.
+# Choose a summary if you need an accurate quantile, no matter what the range and distribution of the values is.
+
 # METRICS - https://github.com/prometheus/client_python
 # Limitations:
 # Registries can not be used as normal, all instantiated metrics are exported
@@ -16,12 +29,24 @@ from prometheus_client import Summary, Histogram, Gauge, Counter
 #  'max': Return a single timeseries that is the maximum of the values of all processes, alive or dead.
 #  'min': Return a single timeseries that is the minimum of the values of all processes, alive or dead.
 
-metrics_req_latency = Histogram('http_request_latency_seconds', 'HTTP Request Latency Seconds')
-metrics_req_count = Counter('http_requests_total',
-                            'Total HTTP Requests (count)',
+metrics_req_latency = Histogram('pequeno_cesar_flask_request_latency_seconds',
+                                'HTTP Request Latency Seconds')
+
+metrics_req_count = Counter('pequeno_cesar_flask_requests_total',
+                            'Total HTTP Requests Count',
                             ['method', 'endpoint', 'status_code'])
-metrics_req_in_progress = Gauge("http_requests_inprogress",
+
+metrics_req_in_progress = Gauge("pequeno_cesar_flask_requests_inprogress",
                                 "Number of in progress HTTP requests",
                                 multiprocess_mode='livesum')
-# metrics_req_time = Summary('request_processing_seconds', 'Time spent processing request')
 
+metrics_query_latency = Histogram('pequeno_cesar_neo4j_request_latency_seconds',
+                                  'Neo4j Bolt Cypher Query Latency Seconds')
+
+metrics_query_count = Counter('pequeno_cesar_neo4j_requests_total',
+                              'Total Neo4j Bolt Cypher Query Count',
+                              ['method', 'endpoint', 'status_code'])
+
+metrics_query_in_progress = Gauge("pequeno_cesar_neo4j_requests_inprogress",
+                                  "Number of in progress Neo4j Bolt Cypher Queries",
+                                  multiprocess_mode='livesum')
